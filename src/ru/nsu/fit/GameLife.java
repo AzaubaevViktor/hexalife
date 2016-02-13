0626f96ece5c5c149f5504c184ae3fed0f62e50b
ktulhy <sarvash.inc@gmail.com>
2016-02-13 19:12:06 +0600

    Умеем рисовать линии

--- src/ru/nsu/fit/GameLife.java
+++ src/ru/nsu/fit/GameLife.java
@@ -4,8 +4,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
 import java.awt.*;
-import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GameLife {
 
@@ -32,12 +32,9 @@ class MyPanel extends JPanel {
 
     private int squareX = 50;
     private int squareY = 50;
-    private int squareW = 20;
-    private int squareH = 20;
     private int hexDiameter = 50;
 
     public MyPanel() {
-
         setBorder(BorderFactory.createLineBorder(Color.black));
 
         addMouseListener(new MouseAdapter() {
@@ -55,7 +52,6 @@ class MyPanel extends JPanel {
     }
 
     private void moveSquare(int x, int y) {
-        int OFFSET = 1;
         if ((squareX != x) || (squareY != y)) {
             repaint();
             squareX = x;
@@ -64,15 +60,52 @@ class MyPanel extends JPanel {
         }
     }
 
-
     public Dimension getPreferredSize() {
         return new Dimension(250, 200);
     }
 
+    public static int getItself(int itself, int dummy)
+    {
+        return itself;
+    }
+
+    private void drawLine(Graphics g, Integer x0, Integer y0, Integer x1, Integer y1, Color color) {
+        boolean steep = Math.abs(y1 - y0) > Math.abs(x1 - x0);
+        if (steep) {
+            //noinspection SuspiciousNameCombination
+            x0 = getItself(y0, y0 = x0);
+            //noinspection SuspiciousNameCombination
+            x1 = getItself(y1, y1 = x1);
+        }
+
+        if (x0 > x1) {
+            x0 = getItself(x1, x1 = x0);
+            y0 = getItself(y1, y1 = y0);
+        }
+
+        int dx = x1 - x0;
+        int dy = Math.abs(y1 - y0);
+        int error = dx / 2;
+
+        int ystep = (y0 < y1) ? 1 : -1;
+
+        int y = y0;
+        for (int x = x0; x <= x1; x++) {
+            int _x = steep ? y : x;
+            int _y = steep ? x : y;
+            g.drawLine(_x, _y, _x, _y);
+            error -= dy;
+            if (error < 0) {
+                y += ystep;
+                error += dx;
+            }
+        }
+    }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawString("This is my custom Panel!", 10, 20);
         drawHexAround(g, squareX, squareY);
+        Graphics2D g2 = (Graphics2D) g;
     }
 
     protected void drawHexAround(Graphics g, int x, int y) {
@@ -81,19 +114,17 @@ class MyPanel extends JPanel {
                 {hexDiameter , 0},
                 {hexDiameter / 2., hexDiameter * sin60},
                 {- hexDiameter / 2., hexDiameter * sin60},
-                {-hexDiameter, 0},
+                {- hexDiameter, 0},
                 {- hexDiameter / 2., - hexDiameter * sin60},
                 {hexDiameter / 2., - hexDiameter * sin60}
         };
         for (int i = 0; i < 7; i++) {
-            // TODO: А не говно ли использовать AWT?
-            Graphics2D g2d = (Graphics2D) g;
-            g2d.setStroke(new BasicStroke(7, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-            g.drawLine(
+            drawLine(g,
                     x + (int) matrix[i % 6][0],
                     y + (int) matrix[i % 6][1],
-                    x + (int) matrix[(i+1) % 6][0],
-                    y + (int) matrix[(i + 1) % 6][1]
+                    x + (int) matrix[(i + 1) % 6][0],
+                    y + (int) matrix[(i + 1) % 6][1],
+                    Color.BLACK
             );
         }
     }
